ec19085414ed7d86ac99ec00d1d44db4ac94205a
Lo0p6TheTardigrade <67520884+Lo0p6TheTardigrade@users.noreply.github.com>
2023-05-19 20:47:43 +0200

    JS edit dot carrousel 2/3

--- assets/script.js
+++ assets/script.js
@@ -4,24 +4,31 @@ const slides = [
     image: 'slide1.jpg',
     tagLine: 'Impressions tous formats <span>en boutique et en ligne</span>',
     // dot: 'dot_selected',
+    dotSel: 'dot_selected',
   },
   {
     image: 'slide2.jpg',
     tagLine: 'Tirages haute définition grand format <span>pour vos bureaux et events</span>',
     // dot: 'dot_selected',
+    dotSel: 'dot_selected',
   },
   {
     image: 'slide3.jpg',
     tagLine: 'Grand choix de couleurs <span>de CMJN aux pantones</span>',
     // dot: 'dot_selected',
+    dotSel: 'dot_selected',
   },
   {
     image: 'slide4.png',
     tagLine: 'Autocollants <span>avec découpe laser sur mesure</span>',
     // dot: 'dot_selected',
+    dotSel: 'dot_selected',
   },
 ];
-console.log(slides);
+// console.log(slides);
+
+// Variable index for slides
+let i = 0;
 
 // Constants for the Arrows
 const arrowLeft = document.querySelector('.arrow_left');
@@ -30,82 +37,61 @@ const arrowRight = document.querySelector('.arrow_right');
 // Create banner img
 const bannerImgCreate = document.getElementById('banner');
 bannerImgCreate.appendChild(document.createElement('img')).classList.add('banner-img');
-console.log(bannerImgCreate);
+// console.log(bannerImgCreate);
 
 // Constants for the dots
-const dots = document.querySelector('.dots');
+const dotsDiv = document.querySelector('.dots');
+// console.log(dotsDiv);
+
+const dots = document.getElementsByClassName('dot');
 console.log(dots);
-const dot = document.querySelector('.dot');
 
-// ForEach for the slides dots
+// ForEach object in slides
 slides.forEach(() => {
-  dots.appendChild(document.createElement('div')).classList.add('dot');
-  // document.querySelector('.dot').classList.add('dot_selected');
+  const dot = document.createElement('div');
+  dot.classList.add('dot');
+  dotsDiv.appendChild(dot);
 });
-
-const dotsArray = [
-  {
-    dot: document.querySelector('.dot').classList.add('dot_selected'),
-  },
-  {
-    dot: document.querySelector('.dot').classList.add('dot_selected'),
-  },
-  {
-    dot: document.querySelector('.dot').classList.add('dot_selected'),
-  },
-  {
-    dot: document.querySelector('.dot').classList.add('dot_selected'),
-  },
-];
-// dotsArray.appendChild(document.createElement('div')).classList.add('dot_selected');
-
-// Variable index for slides
-let i = 0;
+dots[0].classList.add('dot_selected');
 
 // Create src attribute for slideshow
 document.querySelector('.banner-img').setAttribute('src', './assets/images/slideshow/' + slides[0].image);
-// document.querySelector('.dot').classList.add(slides[i].dot);
+document.querySelector('.dot').classList.add(slides[i].dotSel);
 
 arrowLeft.addEventListener('click', () => {
-  i--;
   console.log('click left');
+
+  i--;
+  if (i < 0) {
+    i = slides.length - 1;
+  }
+
   document.querySelector('.banner-img').setAttribute('src', './assets/images/slideshow/' + slides[i].image);
   document.querySelector('#banner p').innerHTML = slides[i].tagLine;
+
+  // ForEach for dots
+  Array.from(dots).forEach((dot) => {
+    dot.classList.remove('dot_selected');
+    console.log(dot);
+  });
+  dots[i].classList.add('dot_selected');
 });
 
 arrowRight.addEventListener('click', () => {
-  i++;
   console.log('click right');
+
+  i++;
+  if (i >= slides.length) {
+    i = 0;
+  }
+
   document.querySelector('.banner-img').setAttribute('src', './assets/images/slideshow/' + slides[i].image);
   document.querySelector('#banner p').innerHTML = slides[i].tagLine;
+
+  // ForEach for dots
+  Array.from(dots).forEach((dot) => {
+    dot.classList.remove('dot_selected');
+    console.log(dot);
+  });
+  dots[i].classList.add('dot_selected');
 });
-
-//
-//
-//
-
-// const bulletPointSelected = document.querySelector('.dots');
-// const bulletPointUnselected = document.querySelector('.dots');
-// const bulletPointSelected = (document.querySelector('.dots').innerHTML = '<div class="dot dot_selected">');
-// const bulletPointUnselected = (document.querySelector('.dots').innerHTML = '<div class="dot">');
-// const bulletPointArray = [
-//   {
-//     pointSelected: bulletPointSelected,
-//   },
-//   {
-//     pointSelectedUnselected: bulletPointUnselected,
-//   },
-// ];
-// console.log(bulletPointArray);
-// slides.concat(bulletPointArray);
-
-// slidesBulletArray = slides.concat(bulletPointArray);
-
-// for (let i = 0; i < slides.length; i++) {
-//   // const element = array[index];
-//   // document.querySelector('.banner-img').innerHTML = '<img src="' + slides.image + '" />';
-//   document.querySelector('.banner-img').innerHTML = slides[i].image;
-//   document.querySelector('#banner p').innerHTML = slides[i].tagLine;
-//   console.log(slides);
-//   document.querySelector('.dots').innerHTML = '<div class="dot "></div><div class="dot dot_selected"></div><div class="dot dot_selected"></div><div class="dot dot_selected"></div>';
-// }
